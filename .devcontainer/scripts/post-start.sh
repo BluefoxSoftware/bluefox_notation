80a9a71e08b67f6767d60f2f60543d1976e29e41
Amu <110311878+AmuLittle@users.noreply.github.com>
2024-05-28 10:53:49 -0400

    Delete .devcontainer directory

--- .devcontainer/scripts/post-start.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-cd libs/main-db
-until edgedb instance start -I $(edgedb project info --instance-name)
-do
-    echo "Error starting instance, retrying..."
-done
-echo "Main database started..."
